85592f08bfe22416757f4e2898351da225589636
Bexultan Myrzatay <bexultan.dev@gmail.com>
2021-09-03 10:10:52 +0600

    Extract Function: volumeCreditsFor

--- main.js
+++ main.js
@@ -25,6 +25,13 @@ function statement (invoice, plays) {
     return result;
   }
 
+  function volumeCreditsFor(perf) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    return volumeCredits;
+  }
+
   let totalAmount = 0;
   let volumeCredits = 0;
 
@@ -35,18 +42,13 @@ function statement (invoice, plays) {
     minimumFractionDigits: 2,
   }).format;
   for (let perf of invoice.performances) {
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
-
+    volumeCredits += volumeCreditsFor(perf);
     // print line for this order
     result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${format(totalAmount/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
-
   return result;
 }
 

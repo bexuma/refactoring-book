c1bd6bb6216ca971c4f55c0aaabee7c99656e539
Bexultan Myrzatay <bexultan.dev@gmail.com>
2021-09-03 11:07:52 +0600

    volumeCredits – Move Function

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function statement (invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
@@ -36,6 +37,13 @@ function statement (invoice, plays) {
     }
     return result;
   }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
 }
 
 function renderPlainText (data) {
@@ -58,7 +66,7 @@ function renderPlainText (data) {
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
     return result;
   }
@@ -70,15 +78,6 @@ function renderPlainText (data) {
       minimumFractionDigits: 2,
     }).format(aNumber / 100);
   }
-  
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
-
-  
 }
 
 module.exports = statement;

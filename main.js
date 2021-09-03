15badcce6b3efaac5ff35fe52c07bb87d6125112
Bexultan Myrzatay <bexultan.dev@gmail.com>
2021-09-03 10:41:29 +0600

    totalVolumeCredits – Extract Function: rename return variable

--- main.js
+++ main.js
@@ -41,11 +41,11 @@ function statement (invoice, plays) {
   }
 
   function totalVolumeCredits() {
-    let volumeCredits = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      result += volumeCreditsFor(perf);
     }
-    return volumeCredits;
+    return result;
   }
 
   let totalAmount = 0;

1a56a60ba5de19fc3af4377f660c64749bd50a9e
Bexultan Myrzatay <bexultan.dev@gmail.com>
2021-09-05 11:52:59 +0600

    PerformanceCalculator – Replace Conditional with Polymorphism: get volumeCredits

--- createStatementData.js
+++ createStatementData.js
@@ -9,10 +9,7 @@ class PerformanceCalculator {
   }
 
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
-    if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
-    return result;
+    return Math.max(this.performance.audience - 30, 0);
   }
 }
 
@@ -35,6 +32,10 @@ class ComedyCalculator extends PerformanceCalculator {
     result += 300 * this.performance.audience;
     return result;
   }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
+  }
 }
 
 export default function createStatementData(invoice, plays) {
@@ -56,7 +57,6 @@ export default function createStatementData(invoice, plays) {
   }
 
   function createPerformanceCalculator(aPerformance, aPlay) {
-    
     switch (aPlay.type) {
       case "tragedy":
         return new TragedyCalculator(aPerformance, aPlay);

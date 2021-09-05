3e515888e5e30e5a4605bce7a99814f4a37d964e
Bexultan Myrzatay <bexultan.dev@gmail.com>
2021-09-05 10:11:59 +0600

    PerformanceCalculator – Inline Function: amount

--- createStatementData.js
+++ createStatementData.js
@@ -39,7 +39,7 @@ export default function createStatementData(invoice, plays) {
     const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
-    result.amount = amountFor(result);
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
 
     return result;
@@ -49,9 +49,6 @@ export default function createStatementData(invoice, plays) {
     return plays[aPerformance.playID];
   }
 
-  function amountFor(aPerformance) {
-    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
-  }
 
   function volumeCreditsFor(aPerformance) {
     let result = 0;
